2d5c6cf08a8bee006c084b934fd055d4787a0bb6
sethmadhu9 <sethmadhu9@gmail.com>
2024-06-08 23:24:52 +0530

    removed local.tf

--- locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  scripts_to_execute = fileset(path.module, "files/*.ps1")
-
-#   command_to_execute = <<EOF
-#     powershell.exe -ExecutionPolicy Unrestricted -Command "${data.template_file.install_devops_agent.rendered} -OrganizationUrl '${var.url}' -Pat '${var.pat}' -Pool '${var.pool}' -AgentName '${var.agent-name}'"
-# EOF
-
-}
